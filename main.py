71c1fcf26fb543464d99d4b6de50ae94968678f8
evgeniikozhanov <evgenii.kozhanov@gmail.com>
2021-07-13 23:47:12 +0300

    cleanup rows by filename after insert

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from tortoise import Tortoise
 from tortoise.exceptions import OperationalError
+from tortoise.transactions import in_transaction
 
 from models import SavedWord
 
@@ -19,17 +20,20 @@ app = FastAPI()
 
 @app.post("/upload/")
 async def upload_words_pairs(upload_words_pairs_request: UploadWordsPairsRequest):
+    inserting_date = datetime.now()
     with open(upload_words_pairs_request.filename, newline='') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
         objects_list = []
         for from_language, to_language, from_word, to_word in csv_reader:
             objects_list.append(SavedWord(from_language=from_language, to_language=to_language, from_word=from_word,
                                           to_word=to_word, words_group_name=upload_words_pairs_request.filename,
-                                          cdate=datetime.now()))
-    try:
-        await SavedWord.bulk_create(objects_list, batch_size=100)
-    except OperationalError as e:
-        return {'ok': False, 'errors': [str(e), ]}
+                                          cdate=inserting_date))
+    async with in_transaction():
+        try:
+            await SavedWord.filter(words_group_name=upload_words_pairs_request.filename).delete()
+            await SavedWord.bulk_create(objects_list, batch_size=100)
+        except OperationalError as e:
+            return {'ok': False, 'errors': [str(e), ]}
 
     return {'ok': True, 'errors': []}
 
